625a46f94c970e598b63b57560af51c8dd946d92
milad <miladtavassoli13@gmail.com>
2023-08-10 15:56:30 +0330

    first commit of new branch

---
+++ src/pages/product-bascket/ProductBascket.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../../Components/pagenav/PageNav";
+
+export default function ProductBascket() {
+  return (
+    <div>
+      <PageNav />
+
+      <h1>ProductBascket</h1>
+    </div>
+  );
+}
---
+++ src/pages/product/Product.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../../Components/pagenav/PageNav";
+
+export default function Product() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Product</h1>
+    </div>
+  );
+}

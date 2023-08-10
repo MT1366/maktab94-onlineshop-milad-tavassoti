eb4a214c44b34afe668526fa562056b0806bc0f5
milad <miladtavassoli13@gmail.com>
2023-08-10 15:36:24 +0330

    some styling for order component were added

--- src/pages/admin-panel/Orders.jsx
+++ src/pages/admin-panel/Orders.jsx
@@ -1,6 +1,6 @@
 export default function Orders() {
   return (
-    <div>
+    <div className="flex flex-row justify-between">
       <h2>Orders Management</h2>
       <button>save</button>
     </div>

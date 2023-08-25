ac97f51d1047716a39483036af11e0f64a61bf98
milad <miladtavassoli13@gmail.com>
2023-08-25 18:39:30 +0330

    implementation of admins dashboard were completed

--- src/Components/tables/OrdersTable.jsx
+++ src/Components/tables/OrdersTable.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOrders } from "../../features/orders/orderSlice";
 import { useEffect } from "react";
-import { Button } from "@material-tailwind/react";
+import { Button, Card, Typography } from "@material-tailwind/react";
 
 export default function OrdersTable() {
   const order = useSelector((store) => store.orders);
@@ -13,22 +13,96 @@ export default function OrdersTable() {
     dispatch(fetchOrders());
   }, []);
 
+  const TABLE_HEAD = ["ProductsName", "Brand", "Price", "Category", "Checking"];
+
   return (
     <main className="flex flex-col ">
       <div className="h-10 w-52 border-bottom p-1">Header</div>
       <div className="h-50 w-52 border p-1">
-        <div>
-          <Button>Delivered</Button>
-          <Button>pendings</Button>
-        </div>
-        <div>
-          {orders &&
-            orders.map((order) => (
-              <>
-                <p key={order.id}>{order.name}</p>
-              </>
-            ))}
-        </div>
+        <Card className="h-full w-51.5 overflow-auto items-center">
+          <table className="w-full min-w-max table-auto text-left">
+            <thead className="text-center">
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map(
+                ({ name, brand, images, price, category, id }, index) => {
+                  const isLast = index === orders.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
+
+                  return (
+                    <>
+                      <tr key={id}>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal text-center"
+                          >
+                            {name}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal text-center"
+                          >
+                            {brand}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal text-center"
+                          >
+                            {price} $
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal text-center"
+                          >
+                            {category}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal text-center"
+                          >
+                            <Button color="blue">STATUS</Button>
+                          </Typography>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </Card>
       </div>
       <div className="h-30 w-52 border p-1">ICONS</div>
     </main>
--- src/Components/tables/ProductsTable.jsx
+++ src/Components/tables/ProductsTable.jsx
@@ -2,12 +2,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../features/products/productSlice";
 import { useEffect } from "react";
 
-import { Card, Typography } from "@material-tailwind/react";
+import { Button, Card, Typography } from "@material-tailwind/react";
 
 export default function ProductsTable() {
   const product = useSelector((store) => store.products);
   const { products } = product;
-  console.log(product);
+  console.log(products);
 
   const dispatch = useDispatch();
 
@@ -15,15 +15,90 @@ export default function ProductsTable() {
     dispatch(fetchProducts());
   }, []);
 
-  const TABLE_HEAD = ["Image", "ProductsName", "Category", ""];
+  const TABLE_HEAD = ["ProductsName", "Image", "Category", "Edit / Delete"];
 
-  const TABLE_ROWS = products;
-  console.log(TABLE_ROWS);
+  // const TABLE_ROWS = products;
+  // console.log(TABLE_ROWS);
 
   return (
     <main className="flex flex-col ">
       <div className="h-10 w-52 border-bottom p-1">Header</div>
-      <div className="h-50 w-52 border p-1"></div>
+      <div className="h-50 w-52 p-1">
+        <Card className="h-full w-51.5 overflow-auto items-center">
+          <table className="w-full min-w-max table-auto text-left">
+            <thead className="text-center">
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {products.map(({ name, icon, id, createdAt }, index) => {
+                const isLast = index === products.length - 1;
+                const classes = isLast
+                  ? "p-4"
+                  : "p-4 border-b border-blue-gray-50";
+
+                return (
+                  <>
+                    <tr key={id}>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {name}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {icon}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          {createdAt}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-center"
+                        >
+                          <Button color="blue">Edit</Button>
+                          <Button color="blue">Delet</Button>
+                        </Typography>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        </Card>
+      </div>
       <div className="h-30 w-52 border p-1">ICONS</div>
     </main>
   );
